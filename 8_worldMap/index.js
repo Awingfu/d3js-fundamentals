65f943489ec33aa5c02bd7925cefa84a0a22af79
Adam Lui <wingfului@gmail.com>
2020-08-08 19:07:05 -0500

    added world map

--- 8_worldMap/index.js
+++ 8_worldMap/index.js
@@ -3,7 +3,36 @@ const svg = d3.select("svg");
 const height = +svg.attr("height");
 const width = +svg.attr("width");
 
-d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
-  .then(data => {
-    console.log(data);
-  })
+// const projection = d3.geoMercator();
+const projection = d3.geoNaturalEarth1();
+const pathGenerator = d3.geoPath().projection(projection);
+const g = svg.append('g');
+
+g.append('path')
+  .attr('class', 'sphere')
+  .attr('d', pathGenerator({type: 'Sphere'}))
+
+g.call(d3.zoom().on('zoom', () => {
+    g.attr('transform', d3.event.transform);
+  }));
+
+Promise.all([
+  d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
+  d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
+]).then(([tsvData, topoJSONData]) => {
+  const countryName = tsvData.reduce((acc, d) => {
+    acc[d.iso_n3] = d.name;
+    return acc;
+  }, {});
+
+  const countries = topojson.feature(topoJSONData, topoJSONData.objects.countries);
+
+  g.selectAll('path')
+    .data(countries.features)
+    .enter().append('path')
+      .attr('class', 'country')
+      .attr('d', d => pathGenerator(d))
+    .append('title')
+      .text(d => countryName[d.id]);
+
+})
